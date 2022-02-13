06368f624ec1c91e02547cfac9604c37372b75ba
Ramandeepsinghgithub <97361910+Ramandeepsinghgithub@users.noreply.github.com>
2022-02-14 03:10:42 +1400

    Add files via upload

---
+++ assignment 3 outcomes.py
@@ -0,0 +1,46 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================================== RESTART: C:\Users\hp\AppData\Local\Programs\Python\Python310\$Q-1 .py ==============================================
+Enter any string:ramandeepsingh
+Enter character to check frequency:a
+a occurs  2 times
+
+=============================================== RESTART: C:\Users\hp\AppData\Local\Programs\Python\Python310\$ q2.py ==============================================
+Give month-4
+Give day-20
+Give year-2003
+Date- 20 / 4 / 2003
+Next Date- 21 / 4 / 2003
+Done!
+
+=============================================== RESTART: C:\Users\hp\AppData\Local\Programs\Python\Python310\$ q4.py ==============================================
+Give a number between 4 and 10 including them-9
+Performance=Excellent
+Letter Grade=A
+
+=============================================== RESTART: C:\Users\hp\AppData\Local\Programs\Python\Python310\$q5.py ===============================================
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+
+=============================================== RESTART: C:\Users\hp\AppData\Local\Programs\Python\Python310\$ q7.py ==============================================
+enter a number:12
+0
+1
+1
+2
+3
+5
+8
+1.0833333333333333
+
+=============================================== RESTART: C:\Users\hp\AppData\Local\Programs\Python\Python310\$q8.py ===============================================
+{1, 3, 4, 6, 8}
+{3, 4, 5, 6, 8, 9, 13, 17}
+{1, 2}
+{4, 5, 6, 7, 8, 9, 10}
+{10, 7}
