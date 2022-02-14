fb9b3ca178763f7b11e05a931f366c0dc4176663
Ramandeepsinghgithub <97361910+Ramandeepsinghgithub@users.noreply.github.com>
2022-02-15 02:26:56 +1400

    Add files via upload

---
+++ assignment 3 output q3.py
@@ -0,0 +1,8 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================================== RESTART: C:\Users\hp\AppData\Local\Programs\Python\Python310\$Q-3 .py ==============================================
+Enter 1 nummbe:2
+Enter 2 number:6
+Enter 3 number:8
+[(2, 4), (6, 36), (8, 64)]
